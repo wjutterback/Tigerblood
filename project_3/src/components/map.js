ac2b277ee2ab83a61fb0bdf243f65d8268777bf5
Will Utterback <uwill08@gmail.com>
2021-05-03 05:16:30 -0500

    Removed react-codemirror package, created own react codemirror

--- project_3/src/components/map.js
+++ project_3/src/components/map.js
@@ -174,8 +174,8 @@ function Map() {
     window.addEventListener(
       'keydown',
       function (e) {
-        // space and arrow keys
-        if ([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        // space (32) and arrow keys
+        if ([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
           e.preventDefault();
         }
       },
