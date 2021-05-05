e5ef591a4e7c7da1462ad96dc942469639dcf1f9
Will Utterback <uwill08@gmail.com>
2021-05-05 17:04:59 -0500

    Test array case fix for boss sprites

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,7 +2,7 @@ export const Home = () => {
   return(
     <>
       <section className="container mx-auto">
-        <h1>Welcome to TigerBlood, the brainchild of Will the Conqueror!</h1>
+        <h1>Welcome to TigerBlood, the brainchild of Will the Conqueror and Fahad the Wizard!</h1>
         <h2>To play the game, press PLAY. Don't forget to bring your coding skills.</h2>
       </section>
     </>
