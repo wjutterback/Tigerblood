9fdbfc2b29d23ad4e1b887638eada1461d2926b6
Will Utterback <uwill08@gmail.com>
2021-05-08 07:09:19 -0500

    Implemented Field of Vision/Fog of war

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,13 +1,15 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import * as ROT from "rot-js";
-import tiles from "../assets/tiles.png";
-import tileMap from "../assets/array/array";
-import gameFuncs from "../assets/js/flavor";
-import CodeBox from "./codemirror";
+import React from 'react';
+import { useState, useEffect } from 'react';
+import * as ROT from 'rot-js';
+import tiles from '../assets/tiles.png';
+import tileMap from '../assets/array/array';
+import gameFuncs from '../assets/js/flavor';
+import CodeBox from './codemirror';
 
 function Map() {
-  const [message, setMessage] = useState("You wake up with a jolt, breathing heavily. What is this place? Why is it so dark? What's going on? Your eyes start to adjust to the dark. You look around confused, as unfamiliar shapes start to appear. You muster up the courage to start moving around. The stone up front might have some clues...");
+  const [message, setMessage] = useState(
+    "You wake up with a jolt, breathing heavily. What is this place? Why is it so dark? What's going on? Your eyes start to adjust to the dark. You look around confused, as unfamiliar shapes start to appear. You muster up the courage to start moving around. The stone up front might have some clues..."
+  );
   const [test, setTest] = useState(false);
   const [door, setDoor] = useState({});
 
@@ -19,8 +21,8 @@ function Map() {
 
   function coolLava() {
     if (lavaCounter === 1) {
-      tileMap[22][15][1] = "n";
-      tileMap[23][15][1] = "n";
+      tileMap[23][15][1] = 'n';
+      tileMap[24][15][1] = 'n';
     }
   }
 
@@ -28,36 +30,36 @@ function Map() {
     console.log(pass);
     if (pass === true) {
       lavaCounter++;
-      tileMap[door.x][door.y][0] = ".";
-      tileMap[door.x][door.y][1] = "U";
+      tileMap[door.x][door.y][0] = '.';
+      tileMap[door.x][door.y][1] = 'U';
       setTest(pass);
       coolLava();
     }
   };
 
   function createMap() {
-    let tileSet = document.createElement("img");
+    let tileSet = document.createElement('img');
     tileSet.src = tiles;
     document.body.appendChild(tileSet);
 
     let options = {
-      layout: "tile",
-      bg: "transparent",
+      layout: 'tile',
+      bg: 'transparent',
       tileWidth: 32,
       tileHeight: 32,
       tileSet: tileSet,
       tileMap: {
-        "@": [96, 2112], // Main Character - Mage
+        '@': [96, 2112], // Main Character - Mage
         R: [992, 32], // README Stone
-        "#": [864, 224], // Wall tile
-        "[": [256, 544], // Shadow_west
-        "]": [1984, 512], // Shadow_east
-        "~": [32, 544], // Shadow_north
+        '#': [864, 224], // Wall tile
+        '[': [256, 544], // Shadow_west
+        ']': [1984, 512], // Shadow_east
+        '~': [32, 544], // Shadow_north
         q: [160, 544], // Shadow_northwest
         e: [96, 544], // Shadow_northeast
         L: [1696, 32], // Door Locked
         U: [1632, 32], // Door Unlocked
-        "&": [992, 864], // Keypad
+        '&': [992, 864], // Keypad
         I: [480, 672], // Torch 1
         i: [544, 672], // Torch 2
         J: [1568, 0], // Flame 1
@@ -86,17 +88,18 @@ function Map() {
         C: [32, 864], // Boss 8 Lower
         F: [1888, 832], // Final Boss Upper
         f: [1856, 832], // Final Boss Lower
-        ".": [1984, 128], // Floor (Passable) 94x63
+        '.': [1984, 128], // Floor (Passable) 94x63
         K: [992, 864], // Keyboard
         H: [1824, 1664], // Bloody Help
         h: [32, 992], // shackled body
+        W: [576, 0], // darkness
       },
       width: 151,
       height: 31,
     };
     let display = new ROT.Display(options);
 
-    let playerPos = { x: 7, y: 3 };
+    let playerPos = { x: 7, y: 4 };
     let deadBodyVar = 0;
     let bloodMessageVar = 0;
     let helpStone = 0;
@@ -106,25 +109,41 @@ function Map() {
     let golemVar = 0;
 
     tileSet.onload = function () {
-      //draw map from array
-      tileMap.forEach((element, y) => {
-        element.forEach((element, x) => {
-          display.draw(x, y, element);
-        });
-      });
+
+      //returns true or false on whether light should pass an object
+      function lightPasses(y, x) {
+        if (Array.isArray(tileMap[x][y]) === true) {
+          const blockLight = ['#', 'L', '&', 'M', 'm', 'K'];
+          if (blockLight.some((tile) => tileMap[x][y].includes(tile))) {
+            return false;
+          }
+        } else if (tileMap[x][y] === '#') {
+          return false;
+        }
+        return true;
+      }
+
+      var fov = new ROT.FOV.PreciseShadowcasting(lightPasses);
+
+      function drawLight() {
+        fov.compute(
+          playerPos.x,
+          playerPos.y,
+          2,
+          function (x, y, r, visibility) {
+            let ch = r ? tileMap[y][x] : '@';
+            display.draw(x, y, ch, '#fff', '#660');
+          }
+        );
+      }
+      drawLight();
 
       async function mapEngine() {
         // this is responsible of watching the player move and updating the display accordingly.
         while (true) {
           await movement();
           display.clear();
-          tileMap.forEach((element, y) => {
-            element.forEach((element, x) => {
-              display.draw(x, y, element);
-            });
-          });
-          display.draw(playerPos.x, playerPos.y, "@");
-
+          drawLight();
         }
       }
       mapEngine();
@@ -134,7 +153,7 @@ function Map() {
         while (!action) {
           await new Promise((resolve) => setTimeout(resolve, 100));
           let e = await new Promise((resolve) => {
-            window.addEventListener("keydown", resolve, { once: true });
+            window.addEventListener('keydown', resolve, { once: true });
           });
           if (
             e.keyCode === 38 ||
@@ -153,42 +172,42 @@ function Map() {
         if (Array.isArray(tileMap[x][y])) {
           let value;
           switch (tileMap[x][y][1]) {
-            case "R":
+            case 'R':
               value = gameFuncs.helpStone(deadBodyVar);
               helpStone = 1;
               setMessage(value);
               return false;
-            case "h":
+            case 'h':
               value = gameFuncs.deadBody(deadBodyVar);
               deadBodyVar = 1;
               setMessage(value);
               return false;
-            case "H":
+            case 'H':
               value = gameFuncs.helpMessage(bloodMessageVar);
               bloodMessageVar++;
               setMessage(value);
               return false;
-            case "L":
+            case 'L':
               value = gameFuncs.door(deadBodyVar);
               setMessage(value);
               setDoor({ x: x, y: y });
               break;
-            case "J":
+            case 'J':
               value = gameFuncs.fire(fireVar);
               fireVar++;
               setMessage(value);
               return false;
-            case "N":
+            case 'N':
               value = gameFuncs.hotLava(hotLavaVar);
               hotLavaVar++;
               setMessage(value);
               return false;
-            case "K":
+            case 'K':
               value = gameFuncs.keypad(keypadVar);
               keypadVar++;
               setMessage(value);
               return false;
-            case "M" || "m":
+            case 'M' || 'm':
               value = gameFuncs.golem(golemVar);
               golemVar++;
               setMessage(value);
@@ -197,8 +216,7 @@ function Map() {
           }
         }
         //movement logic
-        const passable = [".", "n", "U"];
-        passable.some((tile) => tileMap[x][y].includes(tile));
+        const passable = ['.', 'n', 'U'];
         console.log(x, y);
         console.log(tileMap[x][y].length);
         console.log(tileMap[x][y]);
@@ -220,7 +238,7 @@ function Map() {
       }
 
       function handleKey(e) {
-        console.log("triggered HandleKey");
+        console.log('triggered HandleKey');
         var keyCode = [];
         //Arrows keys
         keyCode[38] = 0; // key-up
@@ -233,14 +251,14 @@ function Map() {
         }
         let diff = ROT.DIRS[8][keyCode[code]];
         if (passableCheck(playerPos.x + diff[0], playerPos.y + diff[1])) {
-          setMessage("... time to explore ...")
+          setMessage('... time to explore ...');
           if (helpStone === 1 && deadBodyVar === 1) {
             cryptoCheck();
           }
           playerPos.x += diff[0];
           playerPos.y += diff[1];
-          if (tileMap[3][7][1] === "@") {
-            tileMap[3][7].pop();
+          if (tileMap[4][7][1] === '@') {
+            tileMap[4][7].pop();
           }
           return true;
         } else {
@@ -248,26 +266,39 @@ function Map() {
         }
       }
 
-      let canvas = document.getElementById("map");
+      let canvas = document.getElementById('map');
       canvas.appendChild(display.getContainer());
     };
   }
   return (
     <>
-      <div className="row">
-        <div className="col">
-          <div id="map" style={{height:"1070px", width: "1060px", overflow: "auto"}}>
+      <div className='row'>
+        <div className='col'>
+          <div
+            id='map'
+            style={{ height: '1070px', width: '1060px', overflow: 'auto' }}
+          >
             {/* MAP goes here */}
           </div>
         </div>
-        <div className="col">
-          <div className="row" style={{height: "600px", padding: "15px"}}>
-            <div className="col-sm-12" style={{ fontSize: "30px", fontFamily: "cursive", color: "orange", border: "2px dashed brown", background: "black", padding: "50px" }}>
+        <div className='col'>
+          <div className='row' style={{ height: '600px', padding: '15px' }}>
+            <div
+              className='col-sm-12'
+              style={{
+                fontSize: '30px',
+                fontFamily: 'cursive',
+                color: 'orange',
+                border: '2px dashed brown',
+                background: 'black',
+                padding: '50px',
+              }}
+            >
               {message}
             </div>
           </div>
-          <div className="row" style={{height: "500px", paddingTop: "30px"}}>
-            <div className="col-sm-12">
+          <div className='row' style={{ height: '500px', paddingTop: '30px' }}>
+            <div className='col-sm-12'>
               <CodeBox getTestResult={getTestResult} />
             </div>
           </div>
