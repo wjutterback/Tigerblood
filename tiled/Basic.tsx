67e7b05a9c732104c4a6844a141b9b3ca667ae5f
Will Utterback <uwill08@gmail.com>
2021-05-18 20:09:52 -0500

    Cleaned up file Structure

--- tiled/Basic.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="Basic" tilewidth="32" tileheight="32" tilecount="6080" columns="64">
- <image source="ProjectUtumno_full.png" width="2048" height="3040"/>
-</tileset>
